1293d23aeff868d4949649ec99732ab77d787222
Aman <kushwahaaman2505@gmail.com>
2024-03-28 22:46:03 +0530

    modified model.py models for different datasets

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-class model(nn.Module):
+class model1(nn.Module):
     def __init__(self) -> None:
         super().__init__()
     
@@ -27,4 +27,41 @@ class model(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
 
+        return x
+
+class model2(nn.Module):
+    def __init__(self):
+        super(self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 100)  
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+class model3(nn.Module):
+    def __init__(self):
+        super(self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 200)  # Output classes for Tiny ImageNet is 200
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
